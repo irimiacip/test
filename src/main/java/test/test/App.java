f711457dc63386df7249673593c9c9f4184e7163
Irimia Ciprian Andrei <ciprian.irimia@metrosystems.net>
2019-05-27 11:57:01 +0300

    -- commit third

--- src/main/java/test/test/App.java
+++ src/main/java/test/test/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello again!" );
+        System.out.println( "Hello pentru a treia oara!" );
     }
 }
