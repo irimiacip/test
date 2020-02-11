b81c237a5360b0ef7b06351e6d807427f11ee455
Ciprian Irimia <ciprian.irimia@metrosystems.net>
2020-02-11 18:32:20 +0200

    test

---
+++ 2-ClassesandObjects/test/Class.java
@@ -0,0 +1,26 @@
+package test;
+
+class Persons {
+String name;
+int varsta;
+}
+
+public class Class {
+
+	public static void main(String[] args) {
+		
+		
+		Persons p1=new Persons();
+		p1.name="ion";
+		p1.varsta=23;
+		
+		Persons p2=new Persons();
+		p2.name="vasile";
+		p2.varsta=23;	
+		
+		System.out.println(p2.name);
+		System.out.println(p1.varsta);
+	}
+
+	
+}
