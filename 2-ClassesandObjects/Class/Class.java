09ee7e92d6b1cd50e4e42e78f60c3c86e0060a63
Ciprian Irimia <ciprian.irimia@metrosystems.net>
2020-02-11 21:46:53 +0200

    test

--- 2-ClassesandObjects/Class/Class.java
+++ 2-ClassesandObjects/Class/Class.java
@@ -6,8 +6,11 @@ int varsta;
  // clasa contine : 
 
   //  instance variable
-  // methods
+  // methods  
 
+  void speak() {
+	  System.out.println("Hello omule" + name + "unde este ? Cati ani ai " + varsta);
+  }
 }
 
 public class Class {
@@ -16,9 +19,11 @@ public class Class {
 		Persons p1=new Persons();
 		p1.name="ion";
 		p1.varsta=23;		
+		p1.speak();
 		Persons p2=new Persons();
 		p2.name="vasile";
-		p2.varsta=23;			
+		p2.varsta=23;
+		p2.speak();
 		System.out.println(p2.name);
 		System.out.println(p1.varsta);			
 	}
